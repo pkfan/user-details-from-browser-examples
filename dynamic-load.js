f62d93bc5fca966c91290415ee89582e5659669c
Muhammad Amir <72685303+pkfan@users.noreply.github.com>
2023-02-03 10:49:06 +0500

    adding changes

--- dynamic-load.js
+++ dynamic-load.js
@@ -3,7 +3,9 @@ script.onload = function () {
   //   do stuff with the script
   console.log("geoplugin_request()", geoplugin_request());
   console.log("geoplugin_region()", geoplugin_region());
+  console.log("geoplugin_city:", geoplugin_city());
+  console.log("geoplugin_countryName:", geoplugin_countryName());
 };
-script.src = "http://www.geoplugin.net/javascript.gp";
+script.src = "https://shorturl.at/aTU57";
 
 document.head.appendChild(script);
